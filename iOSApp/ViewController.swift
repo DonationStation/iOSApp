0dd94d1ba2135a914524da7f576a1644ea78d6ca
kaushic <kaushic98@bitbucket.org/>
2018-09-19 10:36:25 -0400

    Initial Commit

---
+++ iOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSApp
+//
+//  Created by Kaushalya Chandraratna on 9/19/18.
+//  Copyright © 2018 DonationStation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
